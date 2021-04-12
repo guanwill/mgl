c358329c2b5396fa71120f8c1c6444d62c0f670b
guanwill <w.guan@hotmail.com>
2021-04-12 21:56:09 +1000

    added authReducer tests

--- src/actions/auth/authActions.test.ts
+++ src/actions/auth/authActions.test.ts
@@ -8,8 +8,11 @@ import {
   setRegisterSuccess,
 } from "./authActions";
 
-const createExpectedAction = (type: AuthActionType, payload: any): IAuthAction => {
-    return {type, payload};
+const createExpectedAction = (
+  type: AuthActionType,
+  payload: any
+): IAuthAction => {
+  return { type, payload };
 };
 
 describe("authActions", () => {
@@ -24,58 +27,73 @@ describe("authActions", () => {
 
   describe("Login", () => {
     it("should call login api and dispatch setLoginSuccess", async () => {
-        const dispatch = jest.fn();
-        const state = jest.fn();
-        const api = {
-          authApi: {
-            login: jest.fn(() => Promise.resolve(mockUserResponse)),
-          },
-        };
-    
-        const expectedAction = createExpectedAction(AuthActionType.LOGIN_USER, mockUserResponse)
-        await callLoginApi(username, password)(dispatch, state, api);
-    
-        expect(api.authApi.login).toBeCalledWith(username, password);
-        expect(dispatch).toBeCalledWith(expectedAction);
-      });
-    
-      it("should create LOGIN_USER action", () => {
-        const expectedAction = createExpectedAction(AuthActionType.LOGIN_USER, mockUserResponse)
-        const action: IAuthAction = setLoginSuccess(mockUserResponse as any);
-        expect(action).toEqual(expectedAction)
-      });
-  })
+      const dispatch = jest.fn();
+      const state = jest.fn();
+      const api = {
+        authApi: {
+          login: jest.fn(() => Promise.resolve(mockUserResponse)),
+        },
+      };
+
+      const expectedAction = createExpectedAction(
+        AuthActionType.LOGIN_USER,
+        mockUserResponse
+      );
+      await callLoginApi(username, password)(dispatch, state, api);
+
+      expect(api.authApi.login).toBeCalledWith(username, password);
+      expect(dispatch).toBeCalledWith(expectedAction);
+    });
+
+    it("should create LOGIN_USER action", () => {
+      const expectedAction = createExpectedAction(
+        AuthActionType.LOGIN_USER,
+        mockUserResponse
+      );
+      const action: IAuthAction = setLoginSuccess(mockUserResponse as any);
+      expect(action).toEqual(expectedAction);
+    });
+  });
 
   describe("Logout", () => {
-      it("should create LOGOUT_USER action", () => {
-        const expectedAction = createExpectedAction(AuthActionType.LOGOUT_USER, mockUserResponse)
-        const action: IAuthAction = logoutUser(mockUserResponse as any);
-        expect(action).toEqual(expectedAction)
-      });
-  })
+    it("should create LOGOUT_USER action", () => {
+      const expectedAction = createExpectedAction(
+        AuthActionType.LOGOUT_USER,
+        mockUserResponse
+      );
+      const action: IAuthAction = logoutUser(mockUserResponse as any);
+      expect(action).toEqual(expectedAction);
+    });
+  });
 
   describe("Register", () => {
     it("should call register api and dispatch setRegisterSuccess", async () => {
-        const mockRegisterResponse = {...mockUserResponse, message: 'Pending'}
-        const dispatch = jest.fn();
-        const state = jest.fn();
-        const api = {
-          authApi: {
-            register: jest.fn(() => Promise.resolve(mockRegisterResponse)),
-          },
-        };
-    
-        const expectedAction = createExpectedAction(AuthActionType.REGISTER_USER, {message: mockRegisterResponse.message})
-        await callRegisterApi(username, password)(dispatch, state, api);
-    
-        expect(api.authApi.register).toBeCalledWith(username, password);
-        expect(dispatch).toBeCalledWith(expectedAction);
-      });
-    
-      it("should create REGISTER_USER action", () => {
-        const expectedAction = createExpectedAction(AuthActionType.REGISTER_USER, mockUserResponse)
-        const action: IAuthAction = setRegisterSuccess(mockUserResponse as any);
-        expect(action).toEqual(expectedAction)
-      });
-  })
+      const mockRegisterResponse = { ...mockUserResponse, message: "Pending" };
+      const dispatch = jest.fn();
+      const state = jest.fn();
+      const api = {
+        authApi: {
+          register: jest.fn(() => Promise.resolve(mockRegisterResponse)),
+        },
+      };
+
+      const expectedAction = createExpectedAction(
+        AuthActionType.REGISTER_USER,
+        { message: mockRegisterResponse.message }
+      );
+      await callRegisterApi(username, password)(dispatch, state, api);
+
+      expect(api.authApi.register).toBeCalledWith(username, password);
+      expect(dispatch).toBeCalledWith(expectedAction);
+    });
+
+    it("should create REGISTER_USER action", () => {
+      const expectedAction = createExpectedAction(
+        AuthActionType.REGISTER_USER,
+        mockUserResponse
+      );
+      const action: IAuthAction = setRegisterSuccess(mockUserResponse as any);
+      expect(action).toEqual(expectedAction);
+    });
+  });
 });
---
+++ src/reducers/auth/authReducer.test.ts
@@ -0,0 +1,32 @@
+import { setLoginSuccess, logoutUser, setRegisterSuccess } from "../../actions/auth/authActions";
+import { initialState } from "../../store";
+import authReducer from "./authReducer";
+
+describe("authReducer", () => {
+    const username = "firstname";
+    const mockUserResponse = {
+        user: { username: username },
+        accesstoken: "token",
+        message: "success",
+    };
+
+  it("should update user information with login details", async () => {
+    const action = setLoginSuccess(mockUserResponse as any);
+    const expectedUpdatedUserInformation = {...initialState.userInformation, ...action.payload};
+    const actualUpdatedUserInformation = authReducer(initialState.userInformation, action);
+    expect(actualUpdatedUserInformation).toEqual(expectedUpdatedUserInformation);
+  });
+
+  it("should clear user information to initialstate", async () => {
+    const action = logoutUser(initialState.userInformation);
+    const actualUpdatedUserInformation = authReducer(initialState.userInformation, action);
+    expect(actualUpdatedUserInformation).toEqual(initialState.userInformation);
+  });
+
+  it("should update user information with registration details", async () => {
+    const action = setRegisterSuccess(mockUserResponse as any);
+    const expectedUpdatedUserInformation = {...initialState.userInformation, message: action.payload.message};
+    const actualUpdatedUserInformation = authReducer(initialState.userInformation, action);
+    expect(actualUpdatedUserInformation).toEqual(expectedUpdatedUserInformation);
+  });
+});
